3ebf45253efdcb3fd5d24b27dde8f1aae24c2b4e
Riccardo Silvi <r.silvi@young.business>
2022-08-23 10:11:38 +0200

    delete START_NOW route

--- src/routes.ts
+++ src/routes.ts
@@ -2,4 +2,3 @@ export const HOME_PATH = "/";
 export const LOGIN_PATH = "/login";
 export const SIGNUP_PATH = "/signup";
 export const MARKETS_PATH = "/markets";
-export const START_NOW_PATH = "/start-now";
