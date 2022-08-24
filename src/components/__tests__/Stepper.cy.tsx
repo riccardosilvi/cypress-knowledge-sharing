f2b5d5c0736581514430d0981b66737f4e602dd7
Riccardo Silvi <r.silvi@young.business>
2022-08-24 16:28:36 +0200

    Stepper component subtracts case test description fix

--- src/components/__tests__/Stepper.cy.tsx
+++ src/components/__tests__/Stepper.cy.tsx
@@ -11,7 +11,7 @@ describe('<Stepper>', () => {
         cy.findByRole("button", { name: "increment"}).click()
         cy.dataCy("counter").contains("1")
     })
-    it('subtracts one to count, when plus button is clicked', () =>{
+    it('subtracts one to count, when minus button is clicked', () =>{
         cy.mount(<Stepper />)
         cy.findByRole("button", { name: "decrement"}).click()
         cy.dataCy("counter").contains("-1")
